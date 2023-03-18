e3fa109ed0ee6baa7e3985307fab55c40551c44f
Dmytro Tilug <tilugdmytro@gmail.com>
2023-03-18 14:07:36 +0200

    change pagination

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -14,7 +14,7 @@ export default function HomePage() {
   const [pageCount, setPageCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const itemsPerPage = 10;
+  const itemsPerPage = 9;
 
   useEffect(() => {
     async function getCourses() {
